4b562e02838c245205b4b11a1a5bcb28a99c96b3
Rafael Torres <rafaeltorres257@gmail.com>
2024-07-20 17:47:56 -0300

    Add function to set event listeners on each square
    The function perpasses the whole array of squares and
    sets an event listener on each one, modifying its color
    when triggered.
    
    This is important for scalability as well, as we aren't
    inserting listeners on each element manually. Although,
    thinking about scalability, it would be interesting to
    consider inserting only one listener that considers the
    target, because it would consume less memory.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-
 let grid = document.querySelector(".grid");
 let arrayOfSquares = [];
 let body = document.querySelector("body");
 
 function generateGrid(){
+  //generate the rows
   for(let i = 0; i<16; i++){
     
     let row = document.createElement("div");
@@ -12,14 +12,30 @@ function generateGrid(){
     
     arrayOfSquares[i] = [];
     
+    //generate the squares
     for(let j = 0; j<16; j++){
     let square = document.createElement("div");
-    square.setAttribute("style", "color: blue; background: green; height:20px; width:20px;");
+    square.setAttribute("class", "square");
+    square.setAttribute("style", "color: blue; background: green; height:20px; width:20px; border: solid black 1px;");
     arrayOfSquares[i][j] = square;
-    console.log(arrayOfSquares);
     row.appendChild(square);
     }
   }
 }
 
+function addEventListeners(){
+
+    for(let i=0; i<16; i++){
+        for (let j = 0; j<16; j++){
+            arrayOfSquares[i][j].addEventListener("mouseover", () => {
+            arrayOfSquares[i][j].setAttribute("style", "background: black; height: 20px; width: 20px; border: solid black 1px;");
+            });
+        }
+    }
+  
+}
+
 generateGrid();
+addEventListeners();
+
+
